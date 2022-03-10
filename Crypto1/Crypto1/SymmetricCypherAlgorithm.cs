2b2f585a3f184b5248df17c252adc10df6266f1e
Ayttekao <60965338+Ayttekao@users.noreply.github.com>
2022-03-10 21:58:15 +0300

    Change Exception

--- Crypto1/Crypto1/SymmetricCypherAlgorithm.cs
+++ Crypto1/Crypto1/SymmetricCypherAlgorithm.cs
@@ -1,7 +1,17 @@
+using Crypto1.Interfaces;
+
 namespace Crypto1
 {
-    public class SymmetricCypherAlgorithm
+    public class SymmetricCypherAlgorithm : ICypherAlgorithm
     {
-        
+        public byte[] Encrypt(byte[] encryptBlock)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public byte[] Decrypt(byte[] decryptBlock)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
